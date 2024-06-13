71c3e45bdac90804a1754b5292e3fd0dc9cea514
Femi Emmanuel Oduola <obafemi@meta.com>
2024-06-13 14:07:23 -0700

    about to replace file

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,26 +2,28 @@
 import './MovieCard.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faEye, faHeart} from "@fortawesome/free-solid-svg"
+import {useState} from "react"
 
 function MovieCard ({movie, onClick}) {
+    const [liked, setLiked] = useState(false);
     // fetchNowPlaying()
     // const movie = props.movie;
+    const handleLikeClick = () => {
+        setLiked(!liked);
+    }
     return (
         <div className="MovieCard" onClick={() => onClick(movie)}>
             <img className='MovieImage' src={"https://image.tmdb.org/t/p/w500"+movie.poster_path} alt="Movie Poster" />
             <h3>{movie.original_title}</h3>
             <p>Rating: {movie.vote_average} </p>
-            <FontAwesomeIcon className="liked" icon={faHeart} color="white" onClick={
-                (liked) => {
-                    if(liked.target.style.color=="white"){
-                        liked.target.style.color=="red";
-                        props.
+            <FontAwesomeIcon className="liked" icon={faHeart} color={liked ? "red" : "white"} onClick={
+                (e) => {
+                    e.stopPropagation();
+                    handleLikeClick();
 
                     }
                 }
-            }>
-                <p> The Lion</p>
-            </div>
+            />
         </div>
     );
 }
